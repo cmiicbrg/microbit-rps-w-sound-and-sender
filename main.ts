f56bd953b5b34518697c3a6a236d4fd8a9d8f247
Christoph Stadlbauer <christoph.stadlbauer@brgenns.ac.at>
2024-02-10 14:38:36 +0100

    Add code after step 15

---
+++ main.ts
@@ -0,0 +1,15 @@
+input.onGesture(Gesture.Shake, function () {
+    hand = randint(1, 3)
+    if (hand == 1) {
+        basic.showIcon(IconNames.SmallSquare)
+    } else if (hand == 2) {
+        basic.showIcon(IconNames.Square)
+    } else {
+        basic.showIcon(IconNames.Scissors)
+    }
+})
+let hand = 0
+basic.showIcon(IconNames.Heart)
+basic.forever(function () {
+	
+})
